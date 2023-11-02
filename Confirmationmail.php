dbe8347c0744cd2ef8f290dfb59a72760e26fa8b
UlrichTinkui <mboumda@yandex.ru>
2023-11-02 11:36:30 +0300

    обновление

--- Confirmationmail.php
+++ Confirmationmail.php
@@ -5,32 +5,34 @@ require './PHPMailer/PHPMailer-PHPMailer-dd01c56/src/PHPMailer.php';
 require './PHPMailer/PHPMailer-PHPMailer-dd01c56/src/SMTP.php';
 require './PHPMailer/PHPMailer-PHPMailer-dd01c56/src/Exception.php';
 
+// Create a new PHPMailer instance
 $mail = new PHPMailer();
-$mail->isSMTP();
-$mail->CharSet = 'UTF-8';
-// $mail->Host = 'smtp.gmail.com';
-// $mail->SMTPAuth = false;
-// $mail->Username = 'mboumdaulrich@gmail.com';
-// $mail->Password = 'MBOUMDA1998raymond.';
-// $mail->SMTPSecure = 'tls';
-// $mail->Port = 25;
-// $mail->SMTPAutoTLS = false;
 
- 
-$mail->Host       = "smtp.gmail.com";    // SMTP server example
-$mail->SMTPDebug  = 0;                     // enables SMTP debug information (for testing)
-$mail->SMTPAuth   = true;                  // enable SMTP authentication
-$mail->Port       = 587;                    // set the SMTP port for the GMAIL server
-$mail->Username   = "mboumdaulrich@gmail.com";            // SMTP account username example
-$mail->Password   = "MBOUMDA1998raymond.";            // SMTP account password example
-
-
-$mail->setFrom('mboumdaulrich@gmail.com', 'Ulrich');
-$mail->addAddress('fuhetienne0@gmail.com', 'etienne');
-$mail->Subject = 'Confirmation de commande';
-$mail->Body = 'Bonjour, Votre commande a été reçue avec succès.';
-$mail->send();
+// Enable SMTP debugging (optional)
 $mail->SMTPDebug = 2;
-   
 
-echo $mail->ErrorInfo;
+// Set the SMTP server and authentication details for Gmail
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = false;
+$mail->Username = 'mboumdaulrich@gmail.com'; // Replace with your Gmail email address
+$mail->Password = 'MBOUMDA1998raymond.'; // Replace with your Gmail password
+$mail->SMTPSecure = 'TLS'; // Enable TLS encryption
+$mail->Port = 25; // TCP port to connect to
+
+// Set the sender and recipient of the email
+$mail->setFrom('mboumdaulrich@gmail.com', 'Ulrich'); // Replace with your name and Gmail email address
+$mail->addAddress('fuhetienne0@gmail.com', 'Etienne'); // Replace with the recipient's email address and name
+
+// Set the email subject and body
+$mail->Subject = 'Test Email';
+$mail->Body = 'This is a test email sent using Gmail SMTP server';
+
+// Send the email
+if ($mail->send()) {
+    echo 'Email sent successfully';
+} else {
+    echo 'Error: ' . $mail->ErrorInfo;
+}
+?>
+
